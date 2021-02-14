8e89698d942c7a56cbdb2f7a679163a93018cfd4
zainab799 <79004929+zainab799@users.noreply.github.com>
2021-02-14 12:16:10 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -11,8 +11,11 @@ public class Main {
     public static void main(String args[]) {
      
         Model model = new Model();               // Create shared database
-        Controller1 c1 = new Controller1(model); // Create first controller
-        Controller2 c2 = new Controller2(model); // Create second controller
+        Controller1 c1 = new Controller1(model,"first Screen"); // Create first controller
+        Controller2 c2 = new Controller2(model,"second Screen"); // Create second controller
+        Controller2 c3 = new Controller2(model,"third Screen"); 
+        Controller2 c4 = new Controller2(model,"fourth Screen"); 
+
         
     } // main
   
